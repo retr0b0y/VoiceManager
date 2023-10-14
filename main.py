1d19808372fb2092c5bef2ed9642c94db6e04fe5
retr0b0y <48183585+retr0b0y@users.noreply.github.com>
2023-10-15 02:25:49 +0300

    Changed the output of the /help command
    Signed-off-by: retr0b0y <48183585+retr0b0y@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,19 +8,40 @@ class MyBot(discord.Bot):
     async def on_ready(self):
         print(f'Logged in as {self.user.name} (id: {self.user.id})')
 
-    def add_custom_commands(self):
-        @self.slash_command(name = "info", description = "Get info about bot")
-        async def info(ctx):
-            await ctx.respond("--- Coming soon ---")
+    def add_commands(self):
+        @self.slash_command(name="help", description="Get information about using the bot")
+        async def help(ctx):
+            embed = discord.Embed(
+                title="VoiceManager Bot Help",
+                description="",
+                color=discord.Colour.blurple(),
+            )
+            embed.add_field(name="➜ What is it?", value="This is a simple Discord bot for voice channel management.", inline=False)
+            embed.add_field(name="➜ Commands", value="", inline=False)
+            embed.add_field(name="Command", value="""
+            `/ping`
+            `/help`
+            """, inline=True)
+            embed.add_field(name="Description", value="""
+            Check the bot's latency
+            Get information about using the bot (this message)
+            """, inline=True)
+ 
+            logo = discord.File("./static/images/logo.png", filename="logo.png")
+            profile = discord.File("./static/images/profile.jpg", filename="profile.jpg")
+            embed.set_author(name="Developer: retr0b0y", icon_url="attachment://profile.jpg")
+            embed.set_thumbnail(url="attachment://logo.png")
+ 
+            await ctx.respond(files=[logo, profile], embed=embed)
 
-        @self.slash_command(name = "ping", description = "Ping the bot")
+        @self.slash_command(name="ping", description="Check the bot's latency.")
         async def ping(ctx):
-            await ctx.respond("Pong (this means that everything is ok)")
+            await ctx.respond(f"Pong! Latency is {round(self.latency * 1000)}ms")
 
 
 def main():
     bot = MyBot()
-    bot.add_custom_commands()
+    bot.add_commands()
     bot.run(os.getenv('DISCORD_TOKEN'))
 
 if __name__ == "__main__":
